c57451ad3d687ca932253513733123c1a1f25898
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-17 23:24:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=2, num2=31
+let multiply = num1*num2
+let random = Math.floor((Math.random() * 10) + 1)
+
+let num3=20, num4=5
+const mod = num3/num4
+
+const max = Math.max(num1, num3, num4)
